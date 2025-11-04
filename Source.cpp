2a2045e124e1843e8a3bf40e17b25e7190b5f6b2
Anna Keeler <a-keeler@onu.edu>
2025-11-03 19:28:30 -0500

    Completed displayRun

--- Source.cpp
+++ Source.cpp
@@ -14,7 +14,18 @@ using namespace std;
 
 //prototypes
  
+void displayRun(int values[], int size);
+
 int main(void) {
+    const int SIZE = 20;
+    int values[SIZE];
+    srand(time(0));
+    for (int i = 0; i < SIZE; i++) {
+        values[i] = rand() % 6 + 1;
+    }
+    displayRun(values, SIZE);
+        
+
    
 
 
@@ -22,6 +33,24 @@ int main(void) {
 
     return 0;
 
+}
+
+
+void displayRun(int values[], int size) {
+    bool isInRun = false;
+    for (int i = 0; i < size; i++) {
+        if (values[i] == values[i+1]&& isInRun == false) {
+                cout << "(";
+                isInRun = true;
+        }
+        cout << values[i];
+        if (values[i] != values[i + 1] && isInRun == true) {
+            isInRun = false;
+            cout << ")";
+        }
+        cout << " ";
+    }
+    return;
 }
 
 
